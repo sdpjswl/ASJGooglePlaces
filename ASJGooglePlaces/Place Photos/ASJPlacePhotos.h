c8b9f8734d8064d4149b089cc5e7aba433aec5b0
sudeep <sudeepjaiswal87@gmail.com>
2016-06-19 12:32:19 +0530

    changed copyright year to 2015

--- ASJGooglePlaces/Place Photos/ASJPlacePhotos.h
+++ ASJGooglePlaces/Place Photos/ASJPlacePhotos.h
@@ -1,7 +1,7 @@
 //
 // ASJPlacePhotos.h
 //
-// Copyright (c) 2014 Sudeep Jaiswal
+// Copyright (c) 2015 Sudeep Jaiswal
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
